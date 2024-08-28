36f56ac000e08c5309f0562fe1423a3ea10cab59
chloe-ui218 <chloerhinan10@gmail.com>
2024-08-28 11:27:23 -1200

    chore: first part of control structures

---
+++ controlStructure.js
@@ -0,0 +1,13 @@
+// if-else cotnrol structure
+let age = 31;
+
+console.log('You are ${age} years old');
+
+if  (age >= 18 && age <= 30){
+    console.log("You are eligigible to vote");
+} else if (age > 30){
+    console.log("You are not eligible to vote because you are over 30 years old");
+}
+else{
+    console.log("You are not eligible to vote because you are under 18 years old");
+}
---
+++ operator.js
@@ -0,0 +1,26 @@
+// Arithmetic operators
+let a = 10;
+let b = 18;
+let result = a + b;
+console.log(result); // 15
+
+// Compound assignment operators
+let x = 10;
+x += 5;
+console.log(x); // 15
+
+// Comparison operators
+let isEqual = (a == b);
+console.log(isEqual); // false
+
+// Logical operators
+let logicalResult = (a > 5 && b < 10);
+console.log(logicalResult); // true
+
+// String operators
+let fullName = "John" + " " + "Doe";
+console.log (fullName); // John Doe
+
+// Ternary operator
+let max = (a < b) ? a : b;
+console.log(max); // 10
