553353b64fe9d5f06922d40375d3f1680f81e46f
chloe-ui218 <chloerhinan10@gmail.com>
2024-08-27 19:33:48 -1200

    feat: created our first project with javascripts

---
+++ intro.js
@@ -0,0 +1,13 @@
+let width = 4;
+let height = 5;
+
+console.log('width: ${width} cm');
+console.log('height: ${height} cm');
+
+// area = width * height
+let area = width * height;
+console.log('Area: ${area} cm2' );
+
+// perimeter = 2 * (width + height)
+let perimeter = 2 * (width + height);
+console.log('Perimeter: ${perimeter} cm');
