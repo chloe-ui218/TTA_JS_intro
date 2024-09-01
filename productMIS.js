cbf8bcb4ed3e2c0cc123c7dc4ccf182436b1d398
chloe-ui218 <chloerhinan10@gmail.com>
2024-08-31 15:12:10 -1200

    chore: understood the use class

--- productMIS.js
+++ productMIS.js
@@ -6,7 +6,13 @@ class product {
     }
 
     getInfo(){
-        console.log('\n\ t Name: ${this.name} \n\t price: ${this.price} \n\t category: ${this.category}');
+        console.log(`\n\ t Name: ${this.name} \n\t price: ${this.price} \n\t category: ${this.category}`);
+    }
+
+    applyDiscount(discountRate){
+        let discount = (this.price * discountRate) / 100;
+        this.price -= discount;
+        console.log(`\n\t New price after ${discountRate}% discount: ${this.price}`); 
     }
 }
 
@@ -14,3 +20,6 @@ let product1 = new product('Laptop', 50000, 'Electronics');
 let product2 = new product('Short', 500, 'Clothing');    
 
 product1.getInfo();
+product1.applyDiscount(10);
+product2.getInfo();
+product2.applyDiscount(20);
