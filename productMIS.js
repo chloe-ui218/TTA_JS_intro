06da4f4b6433107d06313e039da243964c71f030
chloe-ui218 <chloerhinan10@gmail.com>
2024-08-31 16:53:59 -1200

    feat: understood the key concept of oop

--- productMIS.js
+++ productMIS.js
@@ -16,10 +16,32 @@ class product {
     }
 }
 
-let product1 = new product('Laptop', 50000, 'Electronics');
+let product1 = new product('T-shirt', 10000, 'clothing');
 let product2 = new product('Short', 500, 'Clothing');    
 
 product1.getInfo();
 product1.applyDiscount(10);
 product2.getInfo();
 product2.applyDiscount(20);
+
+class Electronics extends product {
+    constructor(name, price, brand, warranty){
+        super(name,price, 'Electronics');
+        this.brand = brand;
+        this.warranty = warranty;
+    }
+
+    getElectronicInfo(){
+            console.log(`\t brand: ${this.brand} \n\t warranty: ${this.warranty}`);
+    }
+
+}
+let product3 = new Electronics('laptop', 50000, 'Dell', '1 year');
+let product4 = new Electronics('Mobile', 20000, 'Samsung', '2 year');
+
+product3.getInfo();
+product3.getElectronicInfo();
+product3.applyDiscount(10);
+product4.getInfo();
+product4.getElectronicInfo();
+product4.applyDiscount(20);
